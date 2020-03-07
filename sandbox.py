4ff29617bee8ad9bf4083edeb72b4d4ee40c87c3
Adam Carroll <Oceloteater@users.noreply.github.com>
2020-03-07 19:48:57 +0200

    Saving exercise and renaming code.py

---
+++ sandbox.py
@@ -0,0 +1,22 @@
+# testing code
+
+# friends = ['Adam', 'Catherine', 'Aaron']
+# lads = [friend for friend in friends if friend.startswith('A')]
+# print(lads)
+# print('ID: '+str(id(friends))+' ID: '+str(id(lads)))
+
+# ages = [
+#     {'name': 'Adam', 'age': 31},
+#     {'name': 'Catherine', 'age': 27},
+#     {'name': 'Aaron', 'age': 25}
+# ]
+# for peeps in ages:
+#     for name, age in peeps.items():
+#         print(f'{name} : {age}')
+
+# sequence = [1, 4, 6, 7]
+# doubled = [double(x) for x in sequence]
+# doubled = map(double, sequence)
+# double = list(map(lambda x: x * 2, sequence))
+
+# print(double)
