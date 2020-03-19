f3e7b43afb79cd47e3a04943cad36d4cf4304624
Adam Carroll <Oceloteater@users.noreply.github.com>
2020-03-20 00:13:42 +0200

    Adding more methods and security features with jwt

--- flask-RESTful/app.py
+++ flask-RESTful/app.py
@@ -1,15 +1,40 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'super_secret_key'
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity)  # auth
 
-class Student(Resource):
+items = []
+
+
+class Item(Resource):
+    @jwt_required()
     def get(self, name):
-        return {'student': name}
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
+
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': 'An item of "{}" already exists'.format(name)}, 400
+
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
 
 
-api.add_resource(Student, '/student/<string:name>')  # http://localhost:5000/student/Adam
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
-app.run(port=5000)
+
+# http://localhost:5000/
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
