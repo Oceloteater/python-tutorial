506c3bba1a71402951fe2b76d14524525d212e59
Adam Carroll <Oceloteater@users.noreply.github.com>
2020-03-21 16:21:35 +0200

    Rewriting security to talk to db to pull creds

--- flask-RESTful/security.py
+++ flask-RESTful/security.py
@@ -1,21 +1,13 @@
+from werkzeug.security import safe_str_cmp
 from user import User
-
-users = [
-   User(1, 'adam', '1234')
-]
-
-
-username_mapping = {u.username: u for u in users}
-user_id_mapping = {u.id: u for u in users}
 
 
 def authenticate(username, password):
-    user = username_mapping.get(username, None)
-    if user and user.password == password:
+    user = User.find_by_username(username)
+    if user and safe_str_cmp(user.password, password):
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return user_id_mapping.get(user_id, None
-                               )
+    return User.find_by_id(user_id)
