1199dd33cd3ae63b8085b6bad06261bdc18b5044
Adam Carroll <Oceloteater@users.noreply.github.com>
2020-03-22 22:34:33 +0200

    Implementing properties for SQLAlchemy in app models

--- flask-SQLAlchemy/app.py
+++ flask-SQLAlchemy/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
+from db import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'super_secret_key'
 api = Api(app)
 
@@ -17,4 +20,5 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    db.init_app(app)
     app.run(port=5000, debug=True)
