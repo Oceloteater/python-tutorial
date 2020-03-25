13b09b4ee5028956054d4f1b29764060f43e0d46
Adam Carroll <Oceloteater@users.noreply.github.com>
2020-03-25 19:44:33 +0200

    Rewriting item list resource to use new db setup

--- flask-SQLAlchemy/resources/item.py
+++ flask-SQLAlchemy/resources/item.py
@@ -70,15 +70,15 @@ class Item(Resource):
 
 class ItemList(Resource):
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+        # connection = sqlite3.connect('data.db')
+        # cursor = connection.cursor()
+        #
+        # query = 'SELECT * FROM items'
+        # result = cursor.execute(query)
+        #
+        # items = []
+        # for row in result:
+        #     items.append({'name': row[0], 'price': row[1]})
+        # connection.close()
 
-        query = 'SELECT * FROM items'
-        result = cursor.execute(query)
-
-        items = []
-        for row in result:
-            items.append({'name': row[0], 'price': row[1]})
-        connection.close()
-
-        return {'items': items}
+        return {'items': [item.json() for item in ItemModel.query.all()]}
